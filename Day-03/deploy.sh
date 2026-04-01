bbd6cdcbf852720009544fc7da382b909f3e7948
akpradhan04 <pradhan.adityak@gmail.com>
2026-04-01 22:13:08 +0545

    Code Deployment

--- Day-03/deploy.sh
+++ Day-03/deploy.sh
@@ -1,17 +1,65 @@
-#!bin/bash
+#!/bin/bash
 
-clone_repository(){
-	echo "Cloning Repository Retail-Data-Analytics..."
-	git clone https://github.com/akpradhan04/Retail-Data-Analytics.git
+clone_repository() {
+    echo "Cloning Repository Retail-Data-Analytics..."
+    git clone https://github.com/akpradhan04/Retail-Data-Analytics.git
 }
 
-create_venv(){
-	echo "Creating Virtual Environment..."
-	cd Retail-Data-Analytics
-	python3 -m venv .myvenv
+create_venv() {
+    echo "Creating Virtual Environment..."
+    python3 -m venv .myvenv
 }
+
+activate_venv(){
+	venv_dir=".myvenv"
+
+	# rm -rf .myvenv
+
+	if [[ !  -d "$venv_dir" ]]; then
+		echo "Virtual Environment Directory '$venv_dir'  Not Found..."
+		exit 1
+	fi
+
+	source "$venv_dir/bin/activate"
+
+	if [[ -z $VIRTUAL_ENV ]];then
+		echo "Failed to Create Virtual Environment..."
+		exit 1
+	fi
+
+	echo "Virtual Environment Activated..."
+	return 0
+
+}
+
 
 echo "********Bash Script Started*************"
-clone_repository
-create_venv
+
+# Clone repo if not exists
+if [ -d "Retail-Data-Analytics" ]; then
+    echo "Repository already exists..."
+    cd Retail-Data-Analytics || exit
+else
+    if clone_repository; then
+        cd Retail-Data-Analytics || exit
+    else
+        echo "Failed to clone repository!"
+        exit 1
+    fi
+fi
+
+# Create virtual environment if not exists
+if [ -d ".myvenv" ]; then
+    echo "Virtual Environment already exists..."
+else
+    if ! create_venv; then
+        echo "Failed to create virtual environment!"
+        exit 1
+    fi
+fi
+
+
+activate_venv
+
+
 echo "********Bash Script Completed***********"
